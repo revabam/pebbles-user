db1b957012a484c40409ac021edd59a730205fb4
BrandonScoggins44 <brandon.scoggins44@gmail.com>
2018-08-09 17:02:44 -0400

    implementing CRUD funtionality

--- pebbles-user/src/main/java/com/revature/controllers/BamUserController.java
+++ pebbles-user/src/main/java/com/revature/controllers/BamUserController.java
@@ -2,10 +2,18 @@ package com.revature.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,10 +28,36 @@ public class BamUserController {
 	@Autowired
 	BamUserService bamUserService;
 	
+	@GetMapping(produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<BamUser>> findAllUsers() {
 		System.out.println("[DEBUG] - in BamUserController.findAllUsers()");
 		List<BamUser> allUsers =  bamUserService.findAllUsers();
 		return new ResponseEntity<List<BamUser>>(allUsers, HttpStatus.OK);
 	}
+	
+	@GetMapping(value="/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<BamUser> findUserById(@PathVariable("id") int id) {
+		System.out.println("[DEBUG] - in BamUserController.findUserById()");
+		
+		BamUser user = bamUserService.findUserById(id);
+		
+		if(user == null) {
+			return new ResponseEntity<BamUser>(HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<BamUser>(user, HttpStatus.OK);
+		}
+	}
 
+	@PostMapping(produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<BamUser> addUser(@Valid @RequestBody BamUser newUser) {
+		System.out.println("[DEBUG] - in BamUserController.addUser()");
+		BamUser user = bamUserService.addUser(newUser);
+		return new ResponseEntity<BamUser>(user, HttpStatus.CREATED);
+	}
+	
+	@PutMapping(produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<BamUser> updateUser(@Valid @RequestBody BamUser updatedUser) {
+		BamUser user = bamUserService.updateUser(updatedUser);
+		return new ResponseEntity<BamUser>(user, HttpStatus.OK);
+	}
 }
--- pebbles-user/src/main/java/com/revature/services/BamUserService.java
+++ pebbles-user/src/main/java/com/revature/services/BamUserService.java
@@ -19,4 +19,22 @@ public class BamUserService {
 	public List<BamUser> findAllUsers() {
 		return bamUserRepo.findAll();
 	}
+	
+	public BamUser findUserById(Integer id) {
+		return bamUserRepo.getOne(id);
+	}
+	
+	public BamUser addUser(BamUser newUser) {
+		return bamUserRepo.save(newUser);
+	}
+	
+	public BamUser updateUser(BamUser updatedUser) {
+		BamUser user = findUserById(updatedUser.getId());
+		
+		if(user == null) {
+			return user;
+		} else {
+			return bamUserRepo.save(updatedUser);
+		}
+	}
 }
