c0bd5fd2b7a127d71aeebe3ab1404f53cc15644f
BrandonScoggins44 <brandon.scoggins44@gmail.com>
2018-08-09 13:40:09 -0400

    finished model

--- pebbles-user/src/main/java/com/revature/models/BamUser.java
+++ pebbles-user/src/main/java/com/revature/models/BamUser.java
@@ -2,9 +2,125 @@ package com.revature.models;
 
 public class BamUser {
 
+	private int id;
 	private String firstname;
 	private String lastname;
 	private String email;
 	private int roleId;
 	private int statusId;
+	
+	public BamUser() {
+		
+	}
+
+	public BamUser(int id, String firstname, String lastname, String email, int roleId, int statusId) {
+		super();
+		this.id = id;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.email = email;
+		this.roleId = roleId;
+		this.statusId = statusId;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+
+	public int getStatusId() {
+		return statusId;
+	}
+
+	public void setStatusId(int statusId) {
+		this.statusId = statusId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstname == null) ? 0 : firstname.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((lastname == null) ? 0 : lastname.hashCode());
+		result = prime * result + roleId;
+		result = prime * result + statusId;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BamUser other = (BamUser) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstname == null) {
+			if (other.firstname != null)
+				return false;
+		} else if (!firstname.equals(other.firstname))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lastname == null) {
+			if (other.lastname != null)
+				return false;
+		} else if (!lastname.equals(other.lastname))
+			return false;
+		if (roleId != other.roleId)
+			return false;
+		if (statusId != other.statusId)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "BamUser [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + ", email=" + email
+				+ ", roleId=" + roleId + ", statusId=" + statusId + "]";
+	}
+	
 }
