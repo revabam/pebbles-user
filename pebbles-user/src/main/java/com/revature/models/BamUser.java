3cf7e8d5efcb6e593b24bef3503c2b22e6fb4916
BrandonScoggins44 <brandon.scoggins44@gmail.com>
2018-08-09 12:03:40 -0400

    created pebbles-user project

---
+++ pebbles-user/src/main/java/com/revature/models/BamUser.java
@@ -0,0 +1,10 @@
+package com.revature.models;
+
+public class BamUser {
+
+	private String firstname;
+	private String lastname;
+	private String email;
+	private int roleId;
+	private int statusId;
+}
