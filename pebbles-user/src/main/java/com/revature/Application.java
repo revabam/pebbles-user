e2cb56fc549a4c08e59454f7c99dbf4fd5eb62e1
BrandonScoggins44 <brandon.scoggins44@gmail.com>
2018-08-10 14:30:44 -0400

    added swagger

--- pebbles-user/src/main/java/com/revature/Application.java
+++ pebbles-user/src/main/java/com/revature/Application.java
@@ -3,12 +3,12 @@ package com.revature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 //import org.springframework.context.annotation.Bean;
 
 //@EnableDiscoveryClient
-@EnableEurekaClient
+//@EnableEurekaClient
 @SpringBootApplication
 public class Application {
 	
