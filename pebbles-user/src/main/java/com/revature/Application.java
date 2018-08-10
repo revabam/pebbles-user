fcd9186d7df04319f18ba24f6c315f1241783528
BrandonScoggins44 <brandon.scoggins44@gmail.com>
2018-08-10 15:59:52 -0400

    updates

--- pebbles-user/src/main/java/com/revature/Application.java
+++ pebbles-user/src/main/java/com/revature/Application.java
@@ -2,23 +2,13 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-//import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
-//import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-//@EnableDiscoveryClient
-//@EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 public class Application {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-/*
-	@Bean
-	public AlwaysSampler defaultSampler() {
-		return new AlwaysSampler();
-	}
-*/
 }
