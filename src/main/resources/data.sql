be9dac3855abf424e0e7462e90973b3440a1fbf4
BrandonScoggins44 <brandon.scoggins44@gmail.com>
2018-08-15 21:46:39 -0400

    H2 in memory db is working

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,23 +1,3 @@
-/*
-Insert mock data into the bam_user table
-*/
-insert into bam_user (id, firstname, lastname, email, role_id, status_id)
-values((SELECT bam_user_seq.nextVal FROM dual), 'Wezley', 'Singleton', 'wsingleton@revature.com', 2, 1);
-insert into bam_user (id, firstname, lastname)
-values((SELECT bam_user_seq.nextVal FROM dual), 'Blake', 'Lavla', 'blakel@revature.com', 2, 1);
-insert into bam_user (id, firstname, lastname)
-values((SELECT bam_user_seq.nextVal FROM dual), 'Steve', 'Sparky', 'steves@revature.com', 2, 1);
-insert into bam_user (id, firstname, lastname)
-values((SELECT bam_user_seq.nextVal FROM dual), 'Dillon', 'Qsee', 'dillonq@revature.com', 2, 1);
-
-/*
-Create role and status lookup tables
-*/
-CREATE TABLE role
-(id INTEGER NOT NULL,
-role VARCHAR(25) UNIQUE,
-PRIMARY KEY (id))
-
 insert into role (id, role)
 values(1, 'manager');
 
@@ -27,14 +7,17 @@ values(2, 'trainer');
 insert into role (id, role)
 values(3, 'associate');
 
-
-CREATE TABLE status
-(id INTEGER NOT NULL,
-status VARCHAR(25) UNIQUE,
-PRIMARY KEY (id))
-
-insert into status (id, role)
+insert into status (id, status)
 values(1, 'active');
 
-insert into status (id, role)
+insert into status (id, status)
 values(2, 'inactive');
+
+insert into bam_user (id, firstname, lastname, email, role_id, status_id)
+values(1, 'Wezley', 'Singleton', 'wsingleton@revature.com', 2, 1);
+insert into bam_user (id, firstname, lastname, email, role_id, status_id)
+values(2, 'Blake', 'Lavla', 'blakel@revature.com', 2, 1);
+insert into bam_user (id, firstname, lastname, email, role_id, status_id)
+values(3, 'Steve', 'Sparky', 'steves@revature.com', 2, 1);
+insert into bam_user (id, firstname, lastname, email, role_id, status_id)
+values(4, 'Dillon', 'Qsee', 'dillonq@revature.com', 2, 1);
