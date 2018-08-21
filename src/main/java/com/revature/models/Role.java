949f3d5cda5a9298bb74fb6796f4b32f9d9ffaed
BrandonScoggins44 <brandon.scoggins44@gmail.com>
2018-08-20 22:45:21 -0400

    finished documentation

--- src/main/java/com/revature/models/Role.java
+++ src/main/java/com/revature/models/Role.java
@@ -1,5 +1,10 @@
 package com.revature.models;
 
+/**
+ * An enum for role. Currently not implemented, and are using integers to track role. 
+ * 
+ * @author Brandon Scoggins, Batch: 1806-Jun18-Java-USF, Trainer: Wezley Singleton
+ */
 public enum Role {
 	MANAGER, TRAINER, ASSOCIATE
 }
--- src/main/java/com/revature/services/BamUserService.java
+++ src/main/java/com/revature/services/BamUserService.java
@@ -15,23 +15,57 @@ public class BamUserService {
 
 	@Autowired
 	BamUserRepository bamUserRepo;
-	
+
+	/**
+	* Retrieves a List of all BamUsers from a database.
+	* 
+	*@return A List of all BamUsers from the database.
+	*@author Brandon Scoggins, Batch: 1806-Jun18-Java-USF, Trainer: Wezley Singleton
+	*/
 	public List<BamUser> findAllUsers() {
 		return bamUserRepo.findAll();
 	}
-	
+
+	/**
+	* Retrieves a BamUser from a database by their id.
+	* 
+	*@param id An Integer that identifies a BamUser
+	*@return A BamUser from a database with a matching id.
+	*@author Brandon Scoggins, Batch: 1806-Jun18-Java-USF, Trainer: Wezley Singleton
+	*/
 	public BamUser findUserById(Integer id) {
 		return bamUserRepo.findBamUserById(id);
 	}
-	
+
+	/**
+	* Adds a new BamUser to a database and generates a unique id.
+	* 
+	*@param newUser The new BamUser that will be added to the database.
+	*@return The BamUser that was added to the database with the generated id.
+	*@author Brandon Scoggins, Batch: 1806-Jun18-Java-USF, Trainer: Wezley Singleton
+	*/
 	public BamUser addUser(BamUser newUser) {
 		return bamUserRepo.save(newUser);
 	}
-	
+
+	/**
+	* Retrieves a BamUser from a database by their email.
+	* 
+	*@param user A BamUser object that at minimum contains an email field.
+	*@return A complete BamUser from a database with the matching email.
+	*@author Brandon Scoggins, Batch: 1806-Jun18-Java-USF, Trainer: Wezley Singleton
+	*/
 	public BamUser loginUser(BamUser user) {
 		return bamUserRepo.findBamUserByEmail(user.getEmail());
 	}
-	
+
+	/**
+	* Updates a BamUser in a database.
+	* 
+	*@param updatedUser A BamUser object that contains updated information.
+	*@return The updated BamUser object as it exist in the database.
+	*@author Brandon Scoggins, Batch: 1806-Jun18-Java-USF, Trainer: Wezley Singleton
+	*/
 	public BamUser updateUser(BamUser updatedUser) {
 		BamUser user = bamUserRepo.findBamUserById(updatedUser.getId());
 		
